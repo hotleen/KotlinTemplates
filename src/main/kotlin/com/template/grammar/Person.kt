9b55a843d0ef2cb467ddbd3b8689935d5a1a1238
huzyu <927499561@qq.com>
2023-11-30 23:17:34 +0800

    feat(1130):添加person注释

--- src/main/kotlin/com/template/grammar/Person.kt
+++ src/main/kotlin/com/template/grammar/Person.kt
@@ -6,28 +6,31 @@ package com.template.grammar
  * @Description:
  **/
 
-/**用户基类**/
-open class Person(name:String){
+/**
+ * 用户基类
+ * 如果一个类要被继承，可以使用 open 关键字进行修饰
+ * **/
+open class Person(name: String) {
     /**次级构造函数**/
-    constructor(name:String,age:Int):this(name){
+    constructor(name: String, age: Int) : this(name) {
         //初始化
         println("-------基类次级构造函数---------")
     }
 }
 
 /**子类继承 Person 类**/
-class Student:Person{
+class Student : Person {
 
     /**次级构造函数**/
-    constructor(name:String,age:Int,no:String,score:Int):super(name,age){
+    constructor(name: String, age: Int, no: String, score: Int) : super(name, age) {
         println("-------继承类次级构造函数---------")
-        println("学生名： ${name}")
-        println("年龄： ${age}")
-        println("学生号： ${no}")
-        println("成绩： ${score}")
+        println("学生名： $name")
+        println("年龄： $age")
+        println("学生号： $no")
+        println("成绩： $score")
     }
 }
 
 fun main(args: Array<String>) {
-    var s =  Student("Runoob", 18, "S12345", 89)
+    var s = Student("Runoob", 18, "S12345", 89)
 }
