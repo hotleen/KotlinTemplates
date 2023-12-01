fde451c9105144b33c758a140fded44c77624efa
huzyu <927499561@qq.com>
2023-12-01 22:44:40 +0800

    feat(1201):Kotlin基础数据

---
+++ src/main/kotlin/com/template/derry/s1/KTBase07.kt
@@ -0,0 +1,32 @@
+package com.template.derry.s1
+
+/**
+ * author: jack hu
+ * @Create: 2023-12-01 21:39
+ * @Description:
+ **/
+class KTBase07 {
+}
+
+/**
+ * Kotlin 内置Java类型
+ * String
+ * Char
+ * Boolean
+ * Int
+ * Double
+ * List
+ * Set
+ * Map
+ */
+fun main() {
+
+    println("Hello World")
+
+    /**
+     * var 变量可读可改 变量名在前 类型在后
+     */
+    var name: String = "derry"
+
+    println(name)
+}
