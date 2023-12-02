e1e7d2b9d1dc759390478510e1be62d6dfe627b2
huzyu <927499561@qq.com>
2023-12-02 09:18:06 +0800

    feat(1202):Kotlin类型推导

---
+++ src/main/kotlin/com/template/derry/s1/KTBase09.kt
@@ -0,0 +1,16 @@
+package com.template.derry.s1
+
+/**
+ * author: jack hu
+ * @Create: 2023-12-02 9:14
+ * @Description: Kotlin语言的类型推导
+ **/
+
+fun main() {
+    // 自动类型推导 实际上String类型声明是多余的
+    val info : String = "Kotlin is useful"
+
+    // 自动推导类型为Int
+    var age = 98
+
+}
