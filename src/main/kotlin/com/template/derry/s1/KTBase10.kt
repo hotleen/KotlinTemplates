35c0c00f48dbcd1ad86208056dce3eacba4ecc2a
huzyu <927499561@qq.com>
2023-12-02 16:27:46 +0800

    feat(1202):Kotlin查看bytecode

--- src/main/kotlin/com/template/derry/s1/KTBase10.kt
+++ src/main/kotlin/com/template/derry/s1/KTBase10.kt
@@ -4,6 +4,7 @@ package com.template.derry.s1
  * author: jack hu
  * @Create: 2023-12-02 9:17
  * @Description: Kotlin 语言编译时常量
+ * 查看 Kotlin bytecode: Tools->Kotlin->Show Kotlin ByteCode
  **/
 
 const val PI = 3.1415 //定义编译时常量，不能作为局部变量
