6006fe2b512c13fad8f80698aeb5c162da7441e9
huzyu <927499561@qq.com>
2023-12-02 08:46:39 +0800

    feat(1202):Kotlin变量及常量

---
+++ src/main/kotlin/com/template/derry/s1/KTBase08.kt
@@ -0,0 +1,12 @@
+package com.template.derry.s1
+
+/**
+ * author: jack hu
+ * @Create: 2023-12-02 8:41
+ * @Description: Kotlin var 变量 val 常量 类似于Java final 修饰常量
+ **/
+
+fun main() {
+    val info : String = "MyConstInfo"
+    println(info)
+}
