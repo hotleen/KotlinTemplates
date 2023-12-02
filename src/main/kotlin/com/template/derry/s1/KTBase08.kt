915763662f7ab42a2c1b7f8e806b523d881256ab
huzyu <927499561@qq.com>
2023-12-02 09:33:31 +0800

    feat(1202):Kotlin常量

--- src/main/kotlin/com/template/derry/s1/KTBase08.kt
+++ src/main/kotlin/com/template/derry/s1/KTBase08.kt
@@ -3,7 +3,7 @@ package com.template.derry.s1
 /**
  * author: jack hu
  * @Create: 2023-12-02 8:41
- * @Description: Kotlin var 变量 val 常量 类似于Java final 修饰常量
+ * @Description: Kotlin var 变量 val 可读变量
  **/
 
 fun main() {
---
+++ src/main/kotlin/com/template/derry/s1/KTBase10.kt
@@ -0,0 +1,16 @@
+package com.template.derry.s1
+
+/**
+ * author: jack hu
+ * @Create: 2023-12-02 9:17
+ * @Description: Kotlin 语言编译时常量
+ **/
+
+const val PI = 3.1415 //定义编译时常量，不能作为局部变量
+
+fun main() {
+
+    var space = PI * 5 * 5
+    println(space)
+    println(space is Double) // is 类似于Java instanceOf
+}
