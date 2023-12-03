ba938194622a561eafab33b33280b53a7ed1ed8d
huzyu <927499561@qq.com>
2023-12-03 11:17:50 +0800

    feat(1203):Unit类型

---
+++ src/main/kotlin/com/template/derry/s1/KTBase19.kt
@@ -0,0 +1,19 @@
+package com.template.derry.s1
+
+/**
+ * author: jack hu
+ * @Create: 2023-12-03 10:01
+ * @Description:
+ **/
+
+fun main() {
+    doWork()
+}
+
+/**
+ * Unit 可以不写
+ * 相当于Java void Unit为Kotlin 类型 不是关键字
+ */
+private fun doWork() : Unit {
+
+}
