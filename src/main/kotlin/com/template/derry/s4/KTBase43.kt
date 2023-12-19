cfea1f569a6ba28b121213b5535e466dc378eb6e
huzyu <927499561@qq.com>
2023-12-19 22:43:55 +0800

    feat(1219):substring

---
+++ src/main/kotlin/com/template/derry/s4/KTBase43.kt
@@ -0,0 +1,11 @@
+package com.template.derry.s4
+
+/**
+ * author: jack hu
+ * @Create: 2023-12-19 22:40
+ * @Description: substring range
+ **/
+fun main() {
+    val info = "hello world"
+    println(info.substring(0 until info.indexOf('r')))
+}
