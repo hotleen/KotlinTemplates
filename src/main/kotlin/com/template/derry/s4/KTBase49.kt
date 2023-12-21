8b735b47b740d572e31498d60c11ae5f885e5a81
huzyu <927499561@qq.com>
2023-12-21 21:39:16 +0800

    feat(1221):double转int

---
+++ src/main/kotlin/com/template/derry/s4/KTBase49.kt
@@ -0,0 +1,14 @@
+package com.template.derry.s4
+
+import kotlin.math.roundToInt
+
+/**
+ * author: jack hu
+ * @Create: 2023-12-21 21:36
+ * @Description: double转int
+ **/
+
+fun main() {
+    println(65.454642.toInt())
+    println(66.8.roundToInt())
+}
