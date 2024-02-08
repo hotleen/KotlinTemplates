68dddeb84d059c0260f2b6503815ecffdc8e6e50
huzyu <927499561@qq.com>
2024-02-08 11:25:59 +0800

    feat(0208):枚举类

---
+++ src/main/kotlin/com/template/derry/s9/KTBase95.kt
@@ -0,0 +1,20 @@
+package com.template.derry.s9
+
+/**
+ * author: jack hu
+ * @Create: 2024-02-08 11:21
+ * @Description:
+ **/
+enum class Week {
+    星期一,
+    星期二,
+    星期三,
+    星期四,
+    星期五,
+    星期六,
+    星期日,
+}
+
+fun main() {
+    println(Week.星期一)
+}
