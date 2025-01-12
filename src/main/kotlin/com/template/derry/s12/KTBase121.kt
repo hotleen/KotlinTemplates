d60ee72c9e00cbb1dcbe4b9b0a802787c19408e6
hotleen <927499561s@gmail.com>
2025-01-12 10:47:13 +0800

    feat(0112):重命名

---
+++ src/main/kotlin/com/template/derry/s12/KTBase121.kt
@@ -0,0 +1,15 @@
+package com.template.derry.s12
+
+import com.template.derry.s11.randomItem as g
+
+/**
+ * author: jack hu
+ * @Create: 2025-01-12 10:34
+ * @Description: 导入函数重命名
+ **/
+
+fun main() {
+    val list: List<String> = listOf("jack", "sheldon", "mary")
+
+    println(list.g())
+}
