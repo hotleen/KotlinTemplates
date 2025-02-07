3116ebc03ff9a8f7a23bb8f3531b4bfd2f11e8c7
hotleen <927499561s@gmail.com>
2025-02-07 21:15:30 +0800

    feat(0207):JvmName修改类名

---
+++ src/main/kotlin/com/template/derry/s13/KTBase131.java
@@ -0,0 +1,15 @@
+package com.template.derry.s13;
+
+/**
+ * author: jack hu
+ *
+ * @Create: 2025-02-07 21:00
+ * @Description:
+ **/
+public class KTBase131 {
+
+    public static void main(String[] args) {
+//        KTBase131Kt.getStudentNameValueInfo("testKt..");
+        StudentInfo.getStudentNameValueInfo("testKt..");
+    }
+}
---
+++ src/main/kotlin/com/template/derry/s13/KTBase131.kt
@@ -0,0 +1,11 @@
+@file:JvmName("StudentInfo")
+
+package com.template.derry.s13
+
+/**
+ * author: jack hu
+ * @Create: 2025-02-07 20:58
+ * @Description: package 包名外 修改KT类名
+ **/
+
+fun getStudentNameValueInfo(str: String) = println(str)
